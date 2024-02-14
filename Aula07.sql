4d49d2aed91060a3f3fb2cc5c93e4a43f1830334
jessifedalto <142901908+jessifedalto@users.noreply.github.com>
2024-02-14 11:54:23 -0300

    Create Aula07.sql

---
+++ Aula07.sql
@@ -0,0 +1,51 @@
+select * from EventoPessoa
+Where IDEventoPessoa = '01GDEQ75T14A'
+
+BEGIN TRANSACTION
+delete from EventoPessoa WHERE IDEventoPessoa = '01GDEQ75T14A'
+
+COMMIT --salva, depois do commit não tem mais rollback
+ROLLBACK -- como se fosse um control z
+
+--Declaração de variáveis---
+DECLARE 
+	@Variavel INT,
+	@Nome Varchar(255),
+	@Idade INT
+
+SET @Variavel = 20
+set @Variavel = (Select Capacidade from Sala Where IDSala = '')
+Select @Variavel = Capacidade from sala where IDSala = ''
+SELECT @Variavel
+
+Trigger --Procedimento realizado automaticamente sempre que ocorre um evento especial no banco de dados
+-- Operações que isso ocorre:
+-- Inserção, Exclusão e Atualizar
+-- Três momento que a Trigger pode ser usada:
+ For -- o gatilho é disparado junto da ação
+ After -- o disparo é realizado após a ação ser concluída
+ Instead of -- seja executado no lugar da ação que o gerou
+
+-- Vantagens: impedir transações inválidas(integridade, segurança); Registro de eventos que ocorreram; Integridade referencial.
+
+CREATE TRIGGER [Nome_Trigger]
+ON [Nome_tabela] -- é a tabela à qual o trigger se aplica
+AFTER DELETE 
+AS
+BEGIN
+	--é possível declarar variáveis
+	-- Inserir, excluir ou alterar dados em outras tabelas.
+END
+
+ALTER TRIGGER fnDelete
+ON PESSOA
+INSTEAD OF DELETE
+AS
+BEGIN
+	DELETE FROM EventoPessoa WHERE IDPessoa = (SELECT IDPessoa FROM DELETED)
+	DELETE FROM Pessoa WHERE IDPessoa = (SELECT IDPessoa FROM DELETED)
+END
+
+DELETE FROM Pessoa WHERE IDPessoa = '031MA0H5D14A'
+
+select * from Pessoa
